5568c9992b2bb687f8f7cf15b927f24bfd3d999b
AbirNangare <abirnangare@gmail.com>
2026-08-18 14:23:24 +0530

    Create Smlst.c

---
+++ dsu/expr3/Smlst.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, smallest;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    smallest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < smallest)
+            smallest = arr[i];
+    }
+
+    printf("Smallest element = %d\n", smallest);
+
+    return 0;
+}
